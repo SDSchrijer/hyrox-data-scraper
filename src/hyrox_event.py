1123754fc9ac067fa6657bad39f496d72041a6d9
Stef Schrijer <schrijer@kyos.com>
2025-01-06 08:41:38 +0100

    wip

--- src/hyrox_event.py
+++ src/hyrox_event.py
@@ -1,8 +1,13 @@
 # Built-in Python packages
 import itertools
 
+# External packages
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
 # Internal packages
 from constants import Division, Gender
+from utils import get_html, remove_prefix
 
 class HyroxEvent:
     def __init__(
@@ -18,18 +23,52 @@ class HyroxEvent:
         self.num_event_participants = dict((x, dict((y, 0) for y in Gender)) for x in Division)
 
 
-    def generate_url(
-            self,
-            page: int,
-            division: Division,
-            gender: Gender
-            ):
+    def get_info(self):
         
+        # Define all combinations of division and gender
+        combinations = list(itertools.product(Division, Gender))
+        pbar = tqdm(combinations, desc="Retrieving participants")
+
+        for division, gender in pbar:
+            page = 1
+
+            while True:
+
+                # Track process of retrieving data
+                pbar.set_postfix({
+                    "Division": division.name,
+                    "Gender": gender.name,
+                    "Page": page
+                })
+
+                # Generate URL for page, division, gender combination
+                url = self.generate_url(
+                    page=page, 
+                    division=division, 
+                    gender=gender)
+
+                # Retrieve html code of selected page
+                html = get_html(url)
+
+                # Change to structured object using Beautifulsoup
+                soup = BeautifulSoup(html, 'html.parser')
+
+                # Get subheading of page
+                h2_title: str = soup.h2.text.strip()
+                h2_title = remove_prefix(h2_title, "Results: ").split(" / HYROX")[0]
+                if h2_title == "General Ranking / All":
+                    break
+
+
+        pass
+
+    def generate_url(
+        self,
+        page: int,
+        division: Division,
+        gender: Gender
+        ):
+
         return f"https://hyrox.r.mikatiming.com/season-{self.season}/?page={page}&event=
         {division.value}_{self.event_id}&num_results=100&pid=list&pidp=start&ranking=time_finish_
         netto&search%5Bsex%5D={gender.value}&search%5Bage_class%5D=%25&search%5Bnation%5D=%25"
-
-
-    def get_info(self):
-
-        
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -11,14 +11,7 @@ def get_html(url: str):
     response = request("GET", url, headers={"Cookie": cookie})
     return response.text
     
-def generate_url(page: int, division: str, gender: str, season: int, event_id: str):
-    return (
-        f"https://hyrox.r.mikatiming.com/season-{season}/?page={page}&event={division}" \
-        f"_{event_id}&num_results=100&pid=list&pidp=start&ranking=time_finish_netto&search" \
-        f"%5Bsex%5D={gender}&search%5Bage_class%5D=%25&search%5Bnation%5D=%25"
-    )
-
-def removeprefix(x: str, prefix: str):
+def remove_prefix(x: str, prefix: str):
     if x.startswith(prefix):
         return x[len(prefix):]
     return x
