efbb776d4cb4941bcf04747a22ee8d1f903c6a10
Stef Schrijer <schrijer@kyos.com>
2025-01-06 08:42:10 +0100

    Build athlete class

---
+++ src/athlete.py
@@ -0,0 +1,32 @@
+# Python built-in packages
+
+# Internal modules
+from src.gender import Gender
+from constants import Division
+
+class Athlete:
+
+    def __init__(
+            self, 
+            id: str,
+            name: str,
+            gender: Gender,
+            division: Division,
+            age_group: str,
+            time: str,
+            link: str
+            ):
+        
+        self.id = id
+        self.name = name
+        self.gender = gender
+        self.division = division
+        self.age_group = age_group
+        self.time = time
+        self.link = link
+        self.ignore = False
+
+    def
+
+    
+        
