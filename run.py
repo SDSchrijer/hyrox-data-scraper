c1f8a1b0284172fc23447d4c3e2b8bb50ca347d7
Stef Schrijer <stefschrijer@hotmail.com>
2025-01-11 23:22:29 +0100

    Add launch file

---
+++ run.py
@@ -0,0 +1,6 @@
+import constants
+from src.__main__ import main
+
+if __name__ == "__main__":
+    print(">> {} v.{} by {} <<".format(constants.MODEL, constants.VERSION, constants.AUTHOR))
+    main()
